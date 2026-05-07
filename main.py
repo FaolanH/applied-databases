7c69e146dde480b23511cc30dbadd5f7a072a5e6
Faolán Hamilton <faolanash01@gmail.com>
2026-05-07 20:07:48 +0000

    Setting up the input structure, as part of section 3.1.3

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 # Author: Faolán Hamilton
 # Brief - Use a python file (.py) to interact with the appdbproj.sql database and the appdbprojNeo4j.json database
 
+# Setting up user input and options for the menu
+def main():
+    speaker_name = "Enter speaker name: "
+    while True:
+        display_menu()
+        choice = input("Please input your choice: ")
+
+        if (choice == "x"):
+            break
+        elif (choice == "1"):
+            speaker = get_name(speaker_name)
+            print(speaker.upper())
+
 # Defining the user options
 def display_menu():
+    print ("Conference Management")
+    print("----------------------")
     print ("")
     print("MENU")
     print("====")
@@ -13,7 +28,12 @@ def display_menu():
     print("5 - Add Attendee Connection")
     print("6 - View Rooms")
     print("x - Exit application")
-    print("Please input your choice:")
 
+def get_name(n):
+    name = input(n)
+    return name
+
+if __name__ == "__main__":
+    main()
 
 
