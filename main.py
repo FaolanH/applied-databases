45dae2d6e2442fb385f6cf29810449e0b66186f4
Faolán Hamilton <faolanash01@gmail.com>
2026-05-07 19:50:28 +0000

    Setting out the main menu as per 3.1.2 of the assignment

---
+++ main.py
@@ -0,0 +1,19 @@
+# Author: Faolán Hamilton
+# Brief - Use a python file (.py) to interact with the appdbproj.sql database and the appdbprojNeo4j.json database
+
+# Defining the user options
+def display_menu():
+    print ("")
+    print("MENU")
+    print("====")
+    print("1 - View Speakers and Sessions")
+    print("2 - View Attendees by Company")
+    print("3 - Add New Attendee")
+    print("4 - View Connected Attendees")
+    print("5 - Add Attendee Connection")
+    print("6 - View Rooms")
+    print("x - Exit application")
+    print("Please input your choice:")
+
+
+
